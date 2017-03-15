e8566a515a36b88c63db5c0536be7de9e48007d9
Joshwin Greene <joshwin.greene@gmail.com>
2017-03-14 23:51:29 -0700

    Project Detail Pages: The last selected section header will be highlighted (shown with full opacity). The first section header is selected by default.

---
+++ js/projectNavFunc.js
@@ -0,0 +1,18 @@
+// Manages which section header should be highlighted (full opacity)
+
+var lastSelectedNavElement = null;
+
+$(document).ready(function() {
+	// Add handlers to all a tags that have the nav-item class
+	$('.nav-item').click(switchNavSelection);
+}); // code is run after the DOM is ready to be manipulated
+
+function switchNavSelection(event) {
+	// Reset the last selected element
+	$('.current-nav-item').css('color', 'rgba(0,0,0,.6)');
+	$('.current-nav-item').removeClass('current-nav-item');
+
+	// Update the selected element
+	$(event.target).css('color', 'rgba(0,0,0,1)');
+	$(event.target).addClass('current-nav-item');
+}
