1e1307c9a80309075bf969908fc204e92c5fc774
Joshwin Greene <joshwin.greene@gmail.com>
2020-03-16 15:10:47 -0700

    Portfolio | Done | 3-16-20

---
+++ js/previewShotHoverFunctionality.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    var previewShots = $('.preview-shot');
+
+	for (var i = 0; i < previewShots.length; i++) {
+        var jQueryObject = previewShots.eq(i);
+        jQueryObject.id = previewShots[i].id;
+		addHoverFunctionality(jQueryObject);
+	} 
+});
+
+function addHoverFunctionality(element) {
+    
+    switch(element.id) {
+        case 'untaboo':
+            addMouseOverAndOut(element, 'untaboo-preview.png', 'untaboo-preview-hover.png');
+            break;
+        case 'myseattle':
+            addMouseOverAndOut(element, 'myseattle-preview.png', 'myseattle-preview-hover.png');
+            break;
+        case 'percy':
+            addMouseOverAndOut(element, 'percy-preview.png', 'percy-preview-hover.png');
+            break;
+        case 'stealth-startup':
+            addMouseOverAndOut(element, 'stealth-startup-preview.png', 'stealth-startup-preview-hover.png');
+            break;
+        case 'pocket-ux-case-study':
+            addMouseOverAndOut(element, 'pocket-ux-case-study-preview.png', 'pocket-ux-case-study-preview-hover.png');
+            break;
+        case 'fresno-transit-free':
+            addMouseOverAndOut(element, 'fresno-transit-free-preview.png', 'fresno-transit-free-preview-hover.png');
+            break;
+        case 'sd-website':
+            addMouseOverAndOut(element, 'sd-website-preview.png', 'sd-website-preview-hover.png');
+            break;
+        case 'misc-design-work':
+            addMouseOverAndOut(element, 'misc-design-work-preview.png', 'misc-design-work-preview-hover.png');
+            break;
+        case 'untaboo-2':
+            addMouseOverAndOut(element, 'untaboo-preview.png', 'untaboo-preview-hover.png');
+            break;
+        case 'myseattle-2':
+            addMouseOverAndOut(element, 'myseattle-preview.png', 'myseattle-preview-hover.png');
+            break;
+        case 'percy-2':
+            addMouseOverAndOut(element, 'percy-preview.png', 'percy-preview-hover.png');
+            break;
+        case 'stealth-startup-2':
+            addMouseOverAndOut(element, 'stealth-startup-preview.png', 'stealth-startup-preview-hover.png');
+            break;
+        case 'pocket-ux-case-study-2':
+            addMouseOverAndOut(element, 'pocket-ux-case-study-preview.png', 'pocket-ux-case-study-preview-hover.png');
+            break;
+        case 'fresno-transit-free-2':
+            addMouseOverAndOut(element, 'fresno-transit-free-preview.png', 'fresno-transit-free-preview-hover.png');
+            break;
+        case 'sd-website-2':
+            addMouseOverAndOut(element, 'sd-website-preview.png', 'sd-website-preview-hover.png');
+            break;
+        case 'misc-design-work-2':
+            addMouseOverAndOut(element, 'misc-design-work-preview.png', 'misc-design-work-preview-hover.png');
+            break;
+        default:
+            break;	
+    }
+}
+
+function addMouseOverAndOut(element, origImg, hoverImg) {
+    var imagePath = "img/";
+
+    element.mouseover(function() {
+        element.attr('src', imagePath + hoverImg);
+    });
+    element.mouseout(function() {
+        element.attr('src', imagePath + origImg);
+    });
+}
