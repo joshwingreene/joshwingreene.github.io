60b50c9218e7014a24618ad1c9fc933511329885
Joshwin Greene <joshwin.greene@gmail.com>
2025-01-30 07:52:25 -0800

    Removed the external essay example since I need to publish an essay on Medium first; Set up Google Analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: 'G-8JEXG54TMH'
     },
     locale: "en-US",
     baseUrl: "joshwingreene.github.io",
